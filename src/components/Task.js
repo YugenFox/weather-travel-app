cc693609b2b73e317a1701224e7084e911a3570c
YugenFox <kalebsday@gmail.com>
2023-07-13 07:21:11 -0500

    task updated -shows weatherData for trip duration

--- src/components/Task.js
+++ src/components/Task.js
@@ -16,7 +16,77 @@ const Task = ({ task, deleteTask, toggleReminder }) => {
         />
       </h3>
 
-      <p>{task.date}</p>
+      <div>
+        <h4>Weather Data</h4>
+        {/* loop through the weather data temperature min/max starting at task.startDateIndex ending at task.endDateIndex have to + 1 since .slice does not keep the last parameter in the return just the one before it*/}
+
+        {task.weatherData.daily.time
+          .slice(task.startDateIndex, task.endDateIndex + 1)
+          .map((time, index) => (
+            <div key={index}>
+              <p>
+                <b>Date: {time.slice(5, 10)}</b>
+              </p>
+              <p>
+                Precipitation Probability Max:{" "}
+                {
+                  task.weatherData.daily.precipitation_probability_max.slice(
+                    task.startDateIndex,
+                    task.endDateIndex + 1
+                  )[index]
+                }{""}
+                {
+                  task.weatherData.daily_units
+                    .precipitation_probability_max
+                }
+              </p>
+              <p>
+                Sunrise:{" "}
+                {
+                  task.weatherData.daily.sunrise.slice(
+                    task.startDateIndex,
+                    task.endDateIndex + 1
+                  )[index].slice(-5)
+                }
+              </p>
+              <p>
+                Sunset:{" "}
+                {
+                  task.weatherData.daily.sunset.slice(
+                    task.startDateIndex,
+                    task.endDateIndex + 1
+                  )[index].slice(-5)
+                }
+              </p>
+              <p>
+                Temperature 2m Min:{" "}
+                {
+                  task.weatherData.daily.temperature_2m_min.slice(
+                    task.startDateIndex,
+                    task.endDateIndex + 1
+                  )[index]
+                }{" "}
+                {
+                  task.weatherData.daily_units
+                    .temperature_2m_min
+                }
+              </p>
+              <p>
+                Temperature 2m Max:{" "}
+                {
+                  task.weatherData.daily.temperature_2m_max.slice(
+                    task.startDateIndex,
+                    task.endDateIndex + 1
+                  )[index]
+                }{" "}
+                {
+                  task.weatherData.daily_units
+                    .temperature_2m_max
+                }
+              </p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
