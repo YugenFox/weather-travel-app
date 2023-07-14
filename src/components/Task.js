3a20e03869f2c609157f5099689ed49a8768ad69
YugenFox <kalebsday@gmail.com>
2023-07-14 09:32:43 -0500

    Cleaned up Task .map code in return

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,6 +3,17 @@ import { TiDelete } from "react-icons/ti";
 const Task = ({ task, deleteTask, toggleReminder }) => {
   //set up api to get long & lat from the location or should I do this when making the task itself....
 
+  const startDateIndex = task.startDateIndex;
+  const endDateIndex = task.endDateIndex;
+  const temperaturesMax = task.weatherData.daily.temperature_2m_max.slice(startDateIndex, endDateIndex + 1);
+  const temperaturesMin = task.weatherData.daily.temperature_2m_min.slice(startDateIndex, endDateIndex + 1);
+  const precipitationProbabilities = task.weatherData.daily.precipitation_probability_max.slice(startDateIndex, endDateIndex + 1);
+  const sunriseTimes = task.weatherData.daily.sunrise.slice(startDateIndex, endDateIndex + 1);
+  const sunsetTimes = task.weatherData.daily.sunset.slice(startDateIndex, endDateIndex + 1);
+  const dailyUnitsTemperature = task.weatherData.daily_units.temperature_2m_min
+  const dailyUnitsPrecipitation = task.weatherData.daily_units.precipitation_probability_max
+
+
   return (
     <div
       className={`task ${task.reminder ? "reminder" : ""}`}
@@ -20,67 +31,30 @@ const Task = ({ task, deleteTask, toggleReminder }) => {
         {/* loop through the weather data temperature min/max starting at task.startDateIndex ending at task.endDateIndex have to + 1 since .slice does not keep the last parameter in the return just the one before it*/}
 
         {task.weatherData.daily.time
-          .slice(task.startDateIndex, task.endDateIndex + 1)
-          .map((time, index) => (
-            <div className="dayCard" key={index}>
-              <p>
-                <b>Date: {time.slice(5, 10)}</b>
-              </p>
-              <p>
+        .slice(startDateIndex, endDateIndex + 1)
+        .map((time, index) => (
+          <div className="dayCard" key={index}>
+            <p>
+              <b>Date: {time.slice(5, 10)}</b>
+            </p>
+            <p>
               {/* Max Temp */}
-                {
-                  task.weatherData.daily.temperature_2m_max.slice(
-                    task.startDateIndex,
-                    task.endDateIndex + 1
-                  )[index]
-                }{" / "}
-
-                {/* Min Temp */}
-                {
-                  task.weatherData.daily.temperature_2m_min.slice(
-                    task.startDateIndex,
-                    task.endDateIndex + 1
-                  )[index]
-                }{" "}
-                {
-                  task.weatherData.daily_units
-                    .temperature_2m_min
-                }
-              </p>
-              <p>
-                %precip:{" "}
-                {
-                  task.weatherData.daily.precipitation_probability_max.slice(
-                    task.startDateIndex,
-                    task.endDateIndex + 1
-                  )[index]
-                }{""}
-                {
-                  task.weatherData.daily_units
-                    .precipitation_probability_max
-                }
-              </p>
-              <p>
-                {/* Sunrise:{" "} */}
-                 
-                {
-                  task.weatherData.daily.sunrise.slice(
-                    task.startDateIndex,
-                    task.endDateIndex + 1
-                  )[index].slice(-5)
-                }{" "}
-                {/* Sunset:{" "} */}
-                
-                {
-                  task.weatherData.daily.sunset.slice(
-                    task.startDateIndex,
-                    task.endDateIndex + 1
-                  )[index].slice(-5)
-                }
-              </p>
-             
-            </div>
-          ))}
+              {temperaturesMax[index]} / {""}
+              {/* Min Temp */}
+              {temperaturesMin[index]}{" "}
+              {dailyUnitsTemperature}
+            </p>
+            <p>
+              %precip: {precipitationProbabilities[index]}
+              {dailyUnitsPrecipitation}
+            </p>
+            <p>
+              {/* Sunrise:{" "} */}
+              {sunriseTimes[index].slice(-5)} {/* Sunset:{" "} */}
+              {sunsetTimes[index].slice(-5)}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
