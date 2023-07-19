858113693ec05bc47b81f0b038b4cf7f1079e131
YugenFox <kalebsday@gmail.com>
2023-07-19 10:35:21 -0500

    Style added to footer
    added Developed by <myName>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright &copy; 2023</p>
+      {/* <p>Copyright &copy; 2023</p> */}
+      <div className='footer_credit'>
+        <p>Developed by <a href="https://kalebsday.netlify.app/" className='author'>Kaleb Day</a></p>
+      </div>
       <Link to="/about" className='footer-link'>About</Link>
     </footer>
   )
